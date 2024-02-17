ea8b9250f990f4512ed8e3ec0da84c66403fdf95
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-02-17 21:14:47 +0100

    __

--- main/resources/views/profile/partials/update-profile-information-form.blade.php
+++ main/resources/views/profile/partials/update-profile-information-form.blade.php
@@ -14,7 +14,7 @@
         @method('put')
 
         <div>
-            <input name="name" class="text-gray-500 w-full p-2 border border-black rounded-lg focus:outline-none focus:border-indigo-500" type="text" placeholder="name">
+            <input name="name" class="text-gray-500 w-full p-2 border border-black rounded-lg focus:outline-none focus:border-indigo-500" value="{{ auth()->user()->name }}" type="text" placeholder="name">
             <div class="text-red-500">
                 @error('name')
                     {{ $message }}
@@ -23,7 +23,7 @@
         </div>
 
         <div>
-            <input name="email" class="text-gray-500 w-full p-2 border border-black rounded-lg focus:outline-none focus:border-indigo-500" type="text" placeholder="email">
+            <input name="email" class="text-gray-500 w-full p-2 border border-black rounded-lg focus:outline-none focus:border-indigo-500" value="{{ auth()->user()->email }}" type="text" placeholder="email">
             <div class="text-red-500">
                 @error('email')
                     {{ $message }}
